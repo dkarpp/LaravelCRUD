da9812d32a8b8161645aaaf2d89851bcc59bf735
Dennis Karptsov <karpdennis@gmail.com>
2023-11-11 16:16:11 -0800

    Created Controller, Migration, Seeder

---
+++ crud-app/database/seeders/SeedProducts.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Product;
+use Faker\Factory;
+
+class SeedProducts extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        //DB::table('products')->truncate();
+        Product::query()->delete();
+
+
+        $faker = Factory::create();
+
+
+        foreach(range(1, 30) as $number){
+            Product::create([
+                'name' => $faker->firstName,
+                'price' => $faker->randomFloat(2, 10, 300),
+                'description' => $faker->sentence(),
+                'item_number' => $faker->randomNumber(5, true),
+                'image' => $faker->imageUrl(640, 480, 'animals', true)
+            ]);
+        }
+        //'name', 'price', 'description', 'item_number', 'image'
+
+    }
+}
