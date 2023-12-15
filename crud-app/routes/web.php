83e711fc294ab72331fb4518ed499020f73960ba
Dennis Karptsov <karpdennis@gmail.com>
2023-12-14 20:19:16 -0800

    Final Assignment AI Poem

--- crud-app/routes/web.php
+++ crud-app/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\AI\Chat;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +17,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+
+    $chat = new Chat();
+
+    $poem = $chat->systemMessage("You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.")->send("Compose a poem that explains the concept of recursion in programming.");
+
+    $sillyPoem = $chat->reply("Cool, can you make it much, much sillier.");
+    return view('welcome', ['poem' => $sillyPoem]);
 });
+
+
 
 Route::resource('products', ProductController::class);
